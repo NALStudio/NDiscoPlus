11f21ff20a73753f28ce18cc691dd00f1fc243e4
Niko Leinonen <niko.a.leinonen@gmail.com>
2024-10-18 22:14:32 +0300

    Update Base.cs

--- NDiscoPlus.Shared/Models/Color/Base.cs
+++ NDiscoPlus.Shared/Models/Color/Base.cs
@@ -1,4 +1,6 @@
-﻿namespace NDiscoPlus.Shared.Models.Color;
+﻿using System.Runtime.CompilerServices;
+
+namespace NDiscoPlus.Shared.Models.Color;
 
 public readonly partial struct NDPColor : IEquatable<NDPColor>
 {
@@ -8,6 +10,19 @@ public readonly partial struct NDPColor : IEquatable<NDPColor>
 
     public NDPColor(double x, double y, double brightness)
     {
+        // I had a crash because we tried to send NaN to Philips Hue
+        // Therefore I added these checks so that if it ever happens again,
+        // I can catch it and actually diagnose the issue
+        // since it was impossible to diagnose from the Philips Hue request
+        ThrowIfNotFinite(x);
+        ThrowIfNotFinite(y);
+        ThrowIfNotFinite(brightness);
+
+        // I could also check if values are in range, but I think that's a bit unnecessary
+        // as the X and Y can in theory be any value since the CIE1931 color space doesn't limit its values into any specific range
+        // Brightness on the other hand, is something we control and should be between 0 and 1.
+        // I decided to not check this as well just to be consistent with XY checking.
+
         X = x;
         Y = y;
         Brightness = brightness;
@@ -41,5 +56,11 @@ public readonly partial struct NDPColor : IEquatable<NDPColor>
     public static bool operator !=(NDPColor left, NDPColor right)
     {
         return !(left == right);
+    }
+
+    private static void ThrowIfNotFinite(double value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
+    {
+        if (!double.IsFinite(value))
+            throw new ArgumentOutOfRangeException(paramName, $"Value must be finite, not: '{value}'");
     }
 }
